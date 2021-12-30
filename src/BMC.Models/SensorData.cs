7edf29a5005f3002c91b69e8796908a6b39a016e
mifmasterz <mifmasterz@yahoo.com>
2021-12-30 08:28:55 +0700

    add dahsboard web

---
+++ src/BMC.Models/SensorData.cs
@@ -0,0 +1,11 @@
+﻿namespace BMC.Models
+{
+    public class SensorData
+    {
+        public float Temp { get; set; }
+        public float Distance { get; set; }
+        public float Pressure { get; set; }
+        public float Lng { get; set; }
+        public float Lat { get; set; }
+    }
+}
